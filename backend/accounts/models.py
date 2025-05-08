73fe6a326c58995ab935fcb6615d5ced5a5e00ee
Wiktor1330 <dragg.tactful244@passmail.net.com>
2025-05-08 18:51:50 +0200

    feat(auth): add User model with UUID primary key

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
+
 
 class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_lenght=100)
+    email = models.EmailField()
     avatar_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.username
