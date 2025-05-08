2f37851b753ea0a26dd2db9b2480d6f5294b9bb6
Wiktor1330 <dragg.tactful244@passmail.net.com>
2025-05-08 19:43:48 +0200

    feat(auth) Create GET endpoint UUID for user

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import User
+from django.http import JsonResponse
 
-# Create your views here.
+def get_user(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+        data = {
+            "id" : str(user.id),
+            "username" : user.username,
+            "email" : user.email,
+        }
+        return JsonResponse(data)
+    
+    except User.DoesNotExist:
+        return JsonResponse({"error" : "User not found"}, status=404)
+    
+
