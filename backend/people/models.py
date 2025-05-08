dca5dbcc44623651657a165330e2e4e9359e53f8
Wiktor1330 <dragg.tactful244@passmail.net.com>
2025-05-08 20:18:51 +0200

    feat(models) Create PeopleBasic model

--- backend/people/models.py
+++ backend/people/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class PeopleBasic(models.Model):
+    SEX_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+
+    name = models.CharField(max_length=256)
+    second_name = models.CharField(max_length=256, blank=True)
+    surname = models.CharField(max_length=256)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES)  # Poprawka
+    birth_date = models.DateField(null=True)
+    description = models.TextField(blank=True)
+    image = models.URLField(blank=True, help_text="Picture of person")
+
+    def __str__(self):
+        return f"{self.name} {self.surname} ({self.get_sex_display()})"
