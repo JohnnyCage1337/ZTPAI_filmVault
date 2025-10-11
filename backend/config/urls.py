03caa9d9a0b952b87d0333878946870ba7ac06f9
JohnnyCage1337 <mariuszonator@gmail.com>
2025-10-11 16:28:33 +0200

    Initial commit for new independent main branch

---
+++ backend/config/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def api_test(request):
+    return JsonResponse({'message': 'FILMVAULT API działa!', 'status': 'OK'})
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/test/', api_test, name='api_test'),
+]
+
+# Obsługa plików media w trybie deweloperskim
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
